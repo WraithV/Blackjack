6a4a03c9b09c0c260f245737530dbea7103e79c4
dreece <dreece@cuttingedge.com.au>
2024-08-14 14:48:05 +1000

    Created cpp gameMode blackjack_GameMode

---
+++ Source/Blackjack_DR/Public/BlackJack_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackJack_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKJACK_DR_API ABlackJack_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
